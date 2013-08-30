7168dfebf9fa6224a2c585f41370408910d4c53c
voice data <voicedata@live.com.au>
2013-08-30 15:28:12 +1000

    init comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BestPractices::Application.config.secret_token = 'cda13aff1fa54796223626249efd4d31ac839011f9869dfcd9cdc74399201b6c731d8dc131e39dd29e139daa124f042d4e87a6eb48c721650ee4cde3435618bb'
